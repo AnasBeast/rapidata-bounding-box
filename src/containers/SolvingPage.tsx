1f64fcbcc1a2eb6935f09a34534b9ee34d0a8468
AnasBeast <boussehminea@gmail.com>
2023-10-27 21:16:54 +0100

    fix assest not loading in production

--- src/containers/SolvingPage.tsx
+++ src/containers/SolvingPage.tsx
@@ -23,7 +23,7 @@ const SolvingPage = () => {
         <div className="mt-4 relative">
             <h2 className="mt-4 font-bold text-4xl text-[#fdfdfd]">Annotate the car!</h2>
             <BoundingBox />
-            <img src={"src/assets/" + boundingBoxData.fileName} className="relative z-0 mt-4 h-[480px] aspect-auto" alt="car image" />
+            <img src={"/" + boundingBoxData.fileName} className="relative z-0 mt-4 h-[480px] aspect-auto" alt="car image" />
             <button className="w-full mt-6 bg-[#1D4ED8] hover:opacity-75 text-[#fdfdfd] font-bold py-2 px-4 rounded">Submit</button>
         </div>
     );
