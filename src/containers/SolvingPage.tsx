5d5e557ce0838a4d3ce2ef8f15317047df03b2ce
AnasBeast <boussehminea@gmail.com>
2023-10-28 15:15:02 +0100

    canvas takes take same dimensions as image

--- src/containers/SolvingPage.tsx
+++ src/containers/SolvingPage.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { BoundingBoxData, getSingleBoundingBox } from "../services/boundingBoxService";
 import BoundingBox from "../components/BoundingBox";
 
 const SolvingPage = () => {
     const [boundingBoxData, setBoundingBoxData] = useState<BoundingBoxData>();
+    const [imgDimensions, setImgDimensions] = useState<{ width: number, height: number } | null>(null);
+    const [imgLoaded, setImgLoaded] = useState<boolean>(false);
 
     useEffect(() => {
         async function fetchData() {
@@ -12,6 +14,20 @@ const SolvingPage = () => {
         }
         fetchData();
     }, []);
+
+    const handleImageLoad = (img: HTMLImageElement) => {
+        setImgDimensions({
+            width: img.width,
+            height: img.height
+        })
+        setImgLoaded(true);
+    }
+
+    const imgRefCallback = useCallback((node: HTMLImageElement | null) => {
+        if (node !== null) {
+            node.onload = () => handleImageLoad(node)
+        }
+    }, [])
 
     if(!boundingBoxData) {
         return (
@@ -22,8 +38,8 @@ const SolvingPage = () => {
     return (
         <div className="mt-4 relative">
             <h2 className="mt-4 font-bold text-4xl text-[#fdfdfd]">Annotate the car!</h2>
-            <BoundingBox />
-            <img src={"/" + boundingBoxData.fileName} className="relative z-0 mt-4 h-[480px] aspect-auto" alt="car image" />
+            {imgLoaded && <BoundingBox width={imgDimensions!.width} height={imgDimensions!.height} />}
+            <img ref={imgRefCallback} src={"/" + boundingBoxData.fileName} className="relative z-0 mt-4 h-[50vh] aspect-auto" alt="car image" />
             <button className="w-full mt-6 bg-[#1D4ED8] hover:opacity-75 text-[#fdfdfd] font-bold py-2 px-4 rounded">Submit</button>
         </div>
     );
